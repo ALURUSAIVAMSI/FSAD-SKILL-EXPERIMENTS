8923f77dff134d328ba546ef330e0d43583a2826
ALURUSAIVAMSI <saivamsi267@gmail.com>
2026-03-12 14:12:57 +0530

    Add files via upload

---
+++ Skill2/spring-mvc-demo3/src/main/java/com/example/demo/SpringMvcDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemo3Application.class, args);
+	}
+
+}
